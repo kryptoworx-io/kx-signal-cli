cf38d20ecfb3be9ffb828c204894f8043eb637cf
Martin Gruscher <martin.gruscher@kryptoworx.io>
2021-09-13 15:29:10 +0200

    fix register and verify

--- src/main/java/io/kryptoworx/signalcli/api/SignalCLI.java
+++ src/main/java/io/kryptoworx/signalcli/api/SignalCLI.java
@@ -4,6 +4,8 @@ package io.kryptoworx.signalcli.api;
 public interface SignalCLI {
 
     void register(String number);
+    void registerWithCaptcha(String phoneNumber, String captcha);
+    void verify(String code);
     void send(String message, String targetNumber);
 
 }
--- src/main/java/io/kryptoworx/signalcli/impl/SignalCLIImpl.java
+++ src/main/java/io/kryptoworx/signalcli/impl/SignalCLIImpl.java
@@ -6,6 +6,9 @@ import java.security.Security;
 import org.asamk.signal.manager.RegistrationManager;
 import org.asamk.signal.util.SecurityProvider;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.whispersystems.signalservice.api.KeyBackupServicePinException;
+import org.whispersystems.signalservice.api.KeyBackupSystemNoDataException;
+import org.whispersystems.signalservice.internal.push.LockedException;
 
 import io.kryptoworx.signalcli.api.SignalCLI;
 
@@ -19,6 +22,9 @@ public class SignalCLIImpl implements SignalCLI {
 			Security.insertProviderAt(new SecurityProvider(), 1);
 		    Security.addProvider(new BouncyCastleProvider());
 			registrationManager = RegistrationManager.initInternal("+4368120784581");
+			
+			var m = loadManager("+4368120784581", dataPath, serviceEnvironment, trustNewIdentity
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -27,12 +33,10 @@ public class SignalCLIImpl implements SignalCLI {
 	
 	
     @Override
-    public void register(final String number) {
-    	System.out.println("in register for number: " + number);
-    	// TODO remove when FCM is integrated.
-    	String captcha = System.getProperty("captcha");
+    public void register(final String phoneNumber) {
+    	System.out.println("in register for number: " + phoneNumber);
     	try {
-			registrationManager.register(false, captcha);
+			registrationManager.register(false, null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,7 +46,32 @@ public class SignalCLIImpl implements SignalCLI {
     @Override
     public void send(final String message, final String targetNumber) {
     	System.out.println("in sending message: ");
-    } 
+    	
+    }
+
+
+	@Override
+	public void verify(String code) {
+		try {
+			registrationManager.verifyAccount(code, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+		
+	}
+
+
+	@Override
+	public void registerWithCaptcha(String phoneNumber, String captcha) {
+		// TODO remove when FCM is integrated.
+    	try {
+			registrationManager.register(false, captcha);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
 
 	
 }
