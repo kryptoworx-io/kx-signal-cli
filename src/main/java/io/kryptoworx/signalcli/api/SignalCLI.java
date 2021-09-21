433d5009865b2f00e245ad93896005868362f1af
Martin Gruscher <martin.gruscher@kryptoworx.io>
2021-09-21 13:56:45 +0200

    fix some errors, add receive and sendReceipt

--- src/main/java/io/kryptoworx/signalcli/api/SignalCLI.java
+++ src/main/java/io/kryptoworx/signalcli/api/SignalCLI.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public interface SignalCLI {
 
-    void register(String number);
+    void register(String number, Boolean voiceVerify);
     void registerWithCaptcha(String phoneNumber, String captcha);
     void verify(String code);
     List<String> getRegisteredPhoneNumbers();
     void send(String message, String targetNumber, String attachmentFileName);
+    void sendReceipt(String receiptNumber);
+    void receive();
 
 }
--- src/main/java/org/asamk/signal/SignalCLIImpl.java
+++ src/main/java/org/asamk/signal/SignalCLIImpl.java
@@ -6,10 +6,12 @@ import java.nio.charset.Charset;
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.asamk.signal.commands.exceptions.CommandException;
@@ -21,6 +23,7 @@ import org.asamk.signal.manager.Manager;
 import org.asamk.signal.manager.NotRegisteredException;
 import org.asamk.signal.manager.PathConfig;
 import org.asamk.signal.manager.RegistrationManager;
+import org.asamk.signal.manager.UntrustedIdentityException;
 import org.asamk.signal.manager.api.Message;
 import org.asamk.signal.manager.api.RecipientIdentifier;
 import org.asamk.signal.manager.config.ServiceConfig;
@@ -34,6 +37,7 @@ import org.asamk.signal.util.CommandUtil;
 import org.asamk.signal.util.ErrorUtils;
 import org.asamk.signal.util.IOUtils;
 import org.asamk.signal.util.SecurityProvider;
+import org.bouncycastle.asn1.x509.Time;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.whispersystems.signalservice.api.KeyBackupServicePinException;
 import org.whispersystems.signalservice.api.KeyBackupSystemNoDataException;
@@ -88,13 +92,14 @@ public class SignalCLIImpl implements SignalCLI {
 	}
 
 	@Override
-	public void register(final String phoneNumber) {
+	public void register(final String phoneNumber, Boolean verifyVoice) {
 		System.out.println("in register for number: " + phoneNumber);
 
 		try {
 			registrationManager = RegistrationManager.initInternal(userName);
-			registrationManager.register(false, null);
+			registrationManager.register(verifyVoice, null);
 			registrationManager.close();
+			registrationManager = null;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -107,8 +112,10 @@ public class SignalCLIImpl implements SignalCLI {
 		try {
 			System.out.println("in sending message: ");
 
-			generalManager = loadManager(userName, getDefaultDataPath(), ServiceEnvironment.LIVE,
-					TrustNewIdentity.ON_FIRST_USE);
+			if (generalManager == null) {
+				generalManager = loadManager(userName, getDefaultDataPath(), ServiceEnvironment.LIVE,
+						TrustNewIdentity.ON_FIRST_USE);
+			}
 			var m = generalManager;
 
 			final var recipientStrings = Collections.singletonList(targetNumber);
@@ -163,19 +170,13 @@ public class SignalCLIImpl implements SignalCLI {
 			} catch (GroupNotFoundException | NotAGroupMemberException | GroupSendingNotAllowedException e) {
 				throw new UserErrorException(e.getMessage());
 			}
-		} catch(Exception ex) {
-			
-			if (generalManager != null) {
-				try {
-					generalManager.close();
-				} catch(IOException ioex) {
-					
-				}
-			}
+		} catch (Exception ex) {
+			// Catch checked exceptions from the underlying API and
+			// re-throw as runtime exception.
 			throw new RuntimeException(ex.getMessage());
-			
+		} finally {
+			closeManager();
 		}
-
 	}
 
 	@Override
@@ -184,6 +185,8 @@ public class SignalCLIImpl implements SignalCLI {
 			registrationManager = RegistrationManager.initInternal(userName);
 			registrationManager.verifyAccount(code, null);
 			registrationManager.close();
+			registrationManager = null;
+			System.out.println("Verified");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -211,6 +214,17 @@ public class SignalCLIImpl implements SignalCLI {
 		}
 	}
 
+	void closeManager() {
+		if (generalManager != null) {
+			try {
+				generalManager.close();
+				generalManager = null;
+			} catch (IOException ioex) {
+
+			}
+		}
+	}
+
 	@Override
 	public List<String> getRegisteredPhoneNumbers() {
 		var registeredUsers = new ArrayList<String>();
@@ -221,13 +235,85 @@ public class SignalCLIImpl implements SignalCLI {
 					registeredUsers.add(u);
 				}
 			});
-			
+
 			return registeredUsers;
-			
+
 		} else {
 			return Collections.emptyList();
 		}
+
+	}
+
+	@Override
+	public void receive() {
+		double timeout = 1;
+		var returnOnTimeout = true;
+		if (timeout < 0) {
+			returnOnTimeout = false;
+			timeout = 3600;
+		}
+		boolean ignoreAttachments = false;
+		try {
+			if (generalManager == null) {
+				generalManager = loadManager(userName, getDefaultDataPath(), ServiceEnvironment.LIVE,
+						TrustNewIdentity.ON_FIRST_USE);
+			}
+
+		} catch (CommandException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		var m = generalManager;
+		try {
+			var outputWriter = new PlainTextWriterImpl(System.out);
+			final var handler = new ReceiveMessageHandler(m, (PlainTextWriter) outputWriter);
+			m.receiveMessages((long) (timeout * 1000), TimeUnit.MILLISECONDS, returnOnTimeout, ignoreAttachments,
+					handler);
+			System.out.println("Receive done.");
+		} catch (IOException e) {
+
+		} finally {
+			closeManager();
+		}
+
+	}
+
+	@Override
+	public void sendReceipt(String recipientNumber) {
+		final var recipientString = recipientNumber;
+		final var ts = new Date().getTime();
+		final var targetTimestamps = Collections.singletonList(1632219260474L);
+		final var type = "read";
+
+		try {
+			generalManager = loadManager(userName, getDefaultDataPath(), ServiceEnvironment.LIVE,
+					TrustNewIdentity.ON_FIRST_USE);
+			final var recipient = CommandUtil.getSingleRecipientIdentifier(recipientString, generalManager.getUsername());
+
+			if (type == null || "read".equals(type)) {
+				generalManager.sendReadReceipt(recipient, targetTimestamps);
+			} else if ("viewed".equals(type)) {
+				generalManager.sendViewedReceipt(recipient, targetTimestamps);
+			} else {
+				throw new UserErrorException("Unknown receipt type: " + type);
+			}
+
+			
+		} catch (CommandException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UntrustedIdentityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			closeManager();
+		}
 		
+
+
 	}
 
 }
